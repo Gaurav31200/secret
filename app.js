bf0c501b24c89d3e38d1bcef519835068152742e
Gaurav-Saini <sainigaurav31200@gmail.com>
2021-05-04 13:55:40 +0530

    Project Completed but fb auth remaining

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 // const saltRounds = 10;
@@ -38,7 +39,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -72,6 +74,19 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
 app.get("/", function (req, res) {
     res.render("home");
 });
@@ -80,6 +95,15 @@ app.get("/auth/google", passport.authenticate('google', { scope: ["profile"] }))
 
 app.get('/auth/google/secrets',
     passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    });
+
+app.get("/auth/facebook", passport.authenticate('facebook', { scope: ["profile"] }));
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: "/login" }),
     function (req, res) {
         // Successful authentication, redirect to secrets.
         res.redirect("/secrets");
@@ -94,8 +118,20 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("login");
     }
@@ -130,6 +166,25 @@ app.post("/login", function (req, res) {
         } else {
             passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            foundUser.secret = submittedSecret;
+            foundUser.save(function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/secrets");
+                }
             });
         }
     });
